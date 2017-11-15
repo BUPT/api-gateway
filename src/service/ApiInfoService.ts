ee74f33d89cf8ce4faf30efa53bf04884c8aeeb6
lym152898 <lin_yi_min@163.com>
2017-11-15 16:47:13 +0800

    添加查询函数

--- src/service/ApiInfoService.ts
+++ src/service/ApiInfoService.ts
@@ -3,10 +3,10 @@ import {ApiInfoModel} from "../model/ApiInfoModel";
 
 class ApiInfoService{
     // 连接数据库
-    private _db:any = new DBConnect().getDB();
+    private _db: any = new DBConnect().getDB();
 
     // 插入数据
-    public async function(data: {[key: string]: string}[]): Promise<void>{
+    public async insert(data: {[key: string]: string}[]): Promise<void>{
         this._db.then(function(db){
             let apiInfoModel: ApiInfoModel = new ApiInfoModel(db);
             apiInfoModel.insert(data, function(err){
@@ -17,6 +17,32 @@ class ApiInfoService{
                     console.log("INSERT DATA INTO API_info SUCCESS");
                 }
             });
+        }).catch(function(err){
+            console.log(err);
         });
     }
+
+    // 根据ID查找API相关数据
+    public async queryById(data: string): Promise<any>{
+        let _this = this;
+        return new Promise(function(resolve, reject){
+            let apiInfos: { [key: string]: string } = null;
+            _this._db.then(function (db) {
+                let apiInfoModel: ApiInfoModel = new ApiInfoModel(db);
+                apiInfoModel.query({ ID: data }, function (err, results) {
+                    if (err) {
+                        reject(err);
+                    }else{
+                        resolve(results);
+                    }
+                });
+            }).catch(function(err){
+                 reject(err);
+            }).catch(function(err){
+                console.log(err);
+                reject(err);
+            });
+        });
+    } 
 }  
+export{ApiInfoService};
