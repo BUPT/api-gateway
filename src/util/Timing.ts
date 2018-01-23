24326665fadce82561f43ac32effddbc7fdea069
chenyuanxing <2251128668@qq.com>
2018-01-23 15:32:56 +0800

    add the last days or hours data in return data

--- src/util/Timing.ts
+++ src/util/Timing.ts
@@ -11,21 +11,71 @@ import {SoursePerformanceModel} from "../model/SoursePerformanceModel"
 */
 class Timing{
     public topPerformanceTimingReset(){
-        var tag = schedule.scheduleJob('10 * * * * *', function(){
+        let tag = schedule.scheduleJob('30 1 * * * *', function(){
             TopPerformanceModel.topPerformance.unitTimeTotleVisit = 0;
         });
         // 可通过cancel取消循环
         // tag.cancel();
+        //更新网关的总访问量的返回链表 topPerformance.totleVisitList[]  totleVisitList[0]为当前总访问量
+        let tag2 = schedule.scheduleJob('0 0 0 * * *', function(){
+            let length = TopPerformanceModel.topPerformance.totleVisitList.length;
+            for(let i = 0;i<length;i++){
+                if(length-i-2<0){
+                    TopPerformanceModel.topPerformance.totleVisitList[length-i-1] = TopPerformanceModel.topPerformance.totleVisit;
+                }else{
+                    TopPerformanceModel.topPerformance.totleVisitList[length-i-1] = TopPerformanceModel.topPerformance.totleVisitList[length-i-2];
+                }
+            }
+
+        });
+        //更新网关的单位时间访问量的返回链表 topPerformance.totleVisitList[] 
+        let tag3 = schedule.scheduleJob('0 0 * * * *', function(){
+            let length = TopPerformanceModel.topPerformance.unitTimeTotleVisitList.length;
+            for(let i = 0;i<length;i++){
+                if(length-i-2<0){
+                    TopPerformanceModel.topPerformance.unitTimeTotleVisitList[length-i-1] = TopPerformanceModel.topPerformance.unitTimeTotleVisit;
+                }else{
+                    TopPerformanceModel.topPerformance.unitTimeTotleVisitList[length-i-1] = TopPerformanceModel.topPerformance.unitTimeTotleVisitList[length-i-2];
+                }
+            }
+        });
     }
     public soursePerformanceTimingReset(){
-        var tag = schedule.scheduleJob('30 1 * * * *', function(){
+        let tag = schedule.scheduleJob('30 1 * * * *', function(){
             SoursePerformanceModel._soursePerformanceMap.forEach(function(value,key){
                 value.unitTimeTotleVisit = 0;
             });
         });
-
         // 可通过cancel取消循环
         // tag.cancel();
+        let tag2 = schedule.scheduleJob('0 0 0 * * *', function(){
+            SoursePerformanceModel._soursePerformanceMap.forEach(function(value,key){
+                let length = value.totleVisitList.length;
+                for(let i = 0;i<length;i++){
+                    if(length-i-2<0){
+                        value.totleVisitList[length-i-1] = value.totleVisit;
+                    }else{
+                        value.totleVisitList[length-i-1] = value.totleVisitList[length-i-2];
+                    }
+                }
+            });
+        });
+        let tag3 = schedule.scheduleJob('0 0 * * * *', function(){
+            SoursePerformanceModel._soursePerformanceMap.forEach(function(value,key){
+                let length = value.unitTimeTotleVisitList.length;
+                for(let i = 0;i<length;i++){
+                    if(length-i-2<0){
+                        value.unitTimeTotleVisitList[length-i-1] = value.unitTimeTotleVisit;
+                    }else{
+                        value.unitTimeTotleVisitList[length-i-1] = value.unitTimeTotleVisitList[length-i-2];
+                    }
+                }
+            });
+        });
+    }
+    public initTiming(){
+        this.topPerformanceTimingReset();
+        this.soursePerformanceTimingReset();
     }
 }
 export{Timing}
