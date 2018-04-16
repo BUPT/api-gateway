68a66196d98cce7e744b5b9651d0c334fd2f9dcc
xxy <1725225464@qq.com>
2018-04-16 14:38:49 +0800

    version management

---
+++ public/API Management System/src/app/testversiondata.ts
@@ -0,0 +1,9 @@
+import { Version } from './version';
+
+export const VERSION: Version[] = [
+  { id: "20180413172420539", notice: '第一次发布', state:false,time:"2018-04-13 17:24:20" },
+  { id: "20180413172420540", notice: '第二次发布', state:false,time:"2018-04-14 17:24:20"  },
+  { id: "20180413172420541", notice: '第三次发布', state:false,time:"2018-04-15 17:24:20" },
+  { id: "20180413172420542", notice: '第四次发布', state:false,time:"2018-04-16 17:24:20"  },
+  { id: "20180413172420543", notice: '第五次发布', state:true,time:"2018-04-17 17:24:20" }
+];
