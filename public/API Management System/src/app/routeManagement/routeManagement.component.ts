b26d7e1e0f4398174e3a55eedcdde0d6df2949bf
xxy <1725225464@qq.com>
2018-04-16 14:36:18 +0800

    route management

--- public/API Management System/src/app/routeManagement/routeManagement.component.ts
+++ public/API Management System/src/app/routeManagement/routeManagement.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { NavComponent } from '../dashboard/nav.component';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { NgClass} from '@angular/common';
+import { NgClass } from '@angular/common';
+import { Location } from '@angular/common';
 declare var $: any;
 
 @Component({
   selector: 'app-routes',
   templateUrl: './routeManagement.component.html',
+  styleUrls: ['./routeManagement.component.css']
 })
 export class routeManagementComponent implements OnInit {
 
 
 
-  constructor (
-                 private parent: NavComponent,
-                 private route: ActivatedRoute,
-                 private router: Router
-    ) {}
+  constructor(
+    private parent: NavComponent,
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location
+  ) { }
 
-    ngOnInit(){
-      this.parent.setActiveByPath(this.parent.routeManagement,"");
-      //this.para = this.parent.para;
+  ngOnInit() {
+    this.parent.setActiveByPath(this.parent.routeManagement, "");
+    //this.para = this.parent.para;
 
-    };
+  };
+
+  modify($event): void {
+    $($event.target).parent().parent().children('td').eq(1).children('textarea').removeAttr("disabled");
+  }
+
+  yes($event): void {
+    var newurl = $($event.target).parent().parent().children('td').eq(1).children('textarea').val();
+    alert("修改成功！");
+    $($event.target).parent().parent().children('td').eq(1).children('textarea').val(newurl);
+    $($event.target).parent().parent().children('td').eq(1).children('textarea').attr("disabled", "disabled");
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
